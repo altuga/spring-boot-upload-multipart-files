0ffc0e5b55ca25b8f4536bee6ad9efc0a3467490
AltugAltintas <altuga@gmail.com>
2022-02-16 08:07:08 +0300

    second commit

---
+++ src/main/java/org/jugistanbul/spring/files/upload/SpringBootUploadFilesApplication.java
@@ -0,0 +1,24 @@
+package org.jugistanbul.spring.files.upload;
+
+import javax.annotation.Resource;
+
+import org.jugistanbul.spring.files.upload.service.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadFilesApplication implements CommandLineRunner {
+  @Resource
+  FilesStorageService storageService;
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootUploadFilesApplication.class, args);
+  }
+
+  @Override
+  public void run(String... arg) throws Exception {
+    storageService.deleteAll();
+    storageService.init();
+  }
+}
---
+++ src/test/java/org/jugistanbul/spring/files/upload/SpringBootUploadFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jugistanbul.spring.files.upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
